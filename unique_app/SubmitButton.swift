5c3a40d19aff33612ad2067d42a6157b526e75ab
XandorNaus <xandor.naus@gmail.com>
2020-04-03 13:15:07 +0200

    Created the button element (SubmitButton) for the LogInScreen

--- unique_app/SubmitButton.swift
+++ unique_app/SubmitButton.swift
@@ -2,20 +2,46 @@
 //  SubmitButton.swift
 //  unique_app
 //
-//  Created by Ivo van der Zee on 03/04/2020.
+//  Created by Xandor Naus on 03/04/2020.
 //  Copyright © 2020 Validata. All rights reserved.
 //
 
 import SwiftUI
 
 struct SubmitButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+      var body: some View {
+            
+          Button(action: {
+            
+              }) {
+        
+             ZStack {
+                       
+                       Image("CONFIRM_BTN")
+                       .resizable()
+                       .renderingMode(.original)
+                       .aspectRatio(contentMode: .fill)
+                        .padding()
+                        .frame(width: 445, height: 100)
+                
+
+                Text("Bevestigen")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding([.top, .leading, .trailing])
+                    .foregroundColor(.white)
+                    
+                   }
+                   .opacity(0.8)
+                   .cornerRadius(10.0)
+                   .padding(-15)
+    
+        }
 }
 
 struct SubmitButton_Previews: PreviewProvider {
     static var previews: some View {
         SubmitButton()
     }
+}
 }
