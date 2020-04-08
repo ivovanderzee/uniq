39a95e7de6f1fc1a44d6e58c36f156c248489cc3
XandorNaus <xandor.naus@gmail.com>
2020-04-08 19:51:36 +0200

    Created and realized the animation for the LoaderStartScreen, and added it to the StartScreen

--- unique_app/LoaderStartScreen.swift
+++ unique_app/LoaderStartScreen.swift
@@ -8,18 +8,58 @@
 
 import SwiftUI
 
+
 struct LoaderStartScreen: View {
+    @State private var fillPoint = 0.0
+    
+    private var animation: Animation {
+        
+        // Animation.basic(duration: 0.8).repeatForever(autoreverses: false)
+        
+  Animation.linear(duration: 0.8)
+    .repeatForever(autoreverses: true)
+        
+        
+    }
+
     var body: some View {
+        Ring(fillPoint: fillPoint).stroke(Color.colorGreen, lineWidth: 7)
+            .frame(width: 60, height: 60)
+            .onAppear() {
+            withAnimation {
+                self.fillPoint =    1.0
+        }
         
-            
-            Image("Loader")
-                .resizable()
-                .renderingMode(.original)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 170, height: 70
+        }
+    
+        }
+}
+struct Ring: Shape {
+    var fillPoint: Double
+    
+    var animatableData: Double {
+        get { return fillPoint }
+        set {fillPoint = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var start:Double = 0
+        let end = 360 * fillPoint
         
+        var path = Path()
         
-   ) }
+        var point = CGPoint(x: rect.size.width/2, y: rect.size.height/2)
+        
+        path.addArc(center:(point),
+                    
+            radius: rect.size.width/2,
+            startAngle: .degrees(start),
+            endAngle: .degrees(end),
+            clockwise: false)
+        
+        return path
+    }
+
 }
 
 struct LoaderStartScreen_Previews: PreviewProvider {
